237cc21366718365f097687a05d4e10b6e8e467b
saikiranmothe <saikiran.mothe@gmail.com>
2017-03-13 12:23:27 -0600

    added indentation

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -6,18 +6,18 @@ import com.google.gson.JsonObject;
 
 public class Application {
 
-    public static void main(String[] args) {
-        Gson builder = new GsonBuilder()
-                .setPrettyPrinting()
-                .create();
+ public static void main(String[] args) {
+  Gson builder = new GsonBuilder()
+    .setPrettyPrinting()
+    .create();
 
-        JsonObject object = new JsonObject();
+  JsonObject object = new JsonObject();
 
-        System.getProperties().stringPropertyNames().iterator().forEachRemaining(propertyName ->
-                object.addProperty(propertyName, System.getProperty(propertyName))
-        );
+  System.getProperties().stringPropertyNames().iterator().forEachRemaining(propertyName ->
+    object.addProperty(propertyName, System.getProperty(propertyName))
+  );
 
-        System.out.println(builder.toJson(object));
-    }
+  System.out.println(builder.toJson(object));
+  }
 
 }
